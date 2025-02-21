6e6d84d7171267f861d0119dc9d2faacdd20b85e
alangogoro <cayne9160@gmail.com>
2025-02-21 22:24:25 +0800

    Fix Create Vue-Router with WebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Menu from '@/components/Menu.vue'
 import IntervalSetting from '@/components/IntervalSetting.vue'
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
